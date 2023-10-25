2b261cf9980d03b4bf1ec970a0794d9c150c858d
Akangkha <akangkhasarkar@gmail.com>
2023-10-25 21:22:57 +0530

    Made changes to the SuggestionBox

--- src/components/SuggestionBox.jsx
+++ src/components/SuggestionBox.jsx
@@ -1,29 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SuggestionCard from "./SuggestionCard";
-
+import axios from "axios";
 const SuggestionBox = () => {
+  const [trends, setTrends] = useState([]);
+
+  const options = {
+    method: "GET",
+    url: "https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/products/list",
+    params: {
+      country: "us",
+      lang: "en",
+      currentpage: "0",
+      pagesize: "10",
+    },
+    headers: {
+      "X-RapidAPI-Key": "45dee0f5bcmshdb1d818c09840ecp1a7aaejsn2e687ba933c3",
+      "X-RapidAPI-Host": "apidojo-hm-hennes-mauritz-v1.p.rapidapi.com",
+    },
+  };
+
+  async function fetchTrends() {
+    try {
+      const response = await axios.request(options);
+      setTrends(response.data.results);
+     
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchTrends();
+  }, []);
+
+  
   return (
     <div className="SuggestionBox">
       <h4>Latest Trends</h4>
       <div className="trends">
-        <SuggestionCard/>
-        <SuggestionCard/>
-        <SuggestionCard/>
-        <SuggestionCard/>
-        <SuggestionCard/>
-        <SuggestionCard/>
-        <SuggestionCard/>
-        <SuggestionCard/>
-       
-    </div>
+        {trends.map((item, index) => (
+          <SuggestionCard name={item.name} url={item.images[0].url}/>
+        ))}
+      </div>
 
-    <div className="popularSuggestion">
-      <h4>Popular Suggestion</h4>
-      <p>Striped shirt dress</p>
-      <p>Satin shirts</p>
-      <p>Denim jumpsuit</p>
-     
-    </div>
+      <div className="popularSuggestion">
+        <h4>Popular Suggestion</h4>
+        <p>Striped shirt dress</p>
+        <p>Satin shirts</p>
+        <p>Denim jumpsuit</p>
+        <p>Leather dresses</p>
+        <p>Solid tshirt</p>
+      </div>
     </div>
   );
 };
--- src/components/SuggestionCard.jsx
+++ src/components/SuggestionCard.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-const SuggestionCard = () => {
+const SuggestionCard = ({name,url}) => {
   return (
     <>
-      <div>
-      <img src="https://images.asos-media.com/products/bolongaro-trevor-shearling-trimmed-leather-jacket-in-black/201691488-1-black" alt="Trending item photos" width="120" style={{margin:"8px"}} draggable="false"></img>
-      <p>hello</p>
+      <div >
+      <img src={url} alt="Trending item photos" width="120" style={{margin:"8px"}} draggable="false"></img>
+      <p>{name}</p>
       </div>
     </>
   );
